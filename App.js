8b43fb64658b90a198a34bee7f0620f9533aa7ad
rafay <rafay.asad@mmcgbl.com>
2023-01-02 23:59:24 +0500

    changes: map

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import MapView, { enableLatestRenderer , PROVIDER_GOOGLE } from 'react-native-maps';
+// import  {  } from 'react-native-maps';
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    // height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 function App() {
   return (
-    <View>
-      <Text>hello</Text>
+    <View style={styles.container}>
+      <MapView
+        // provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      >
+      </MapView>
     </View>
   )
 }
