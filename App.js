43d4784837d00a153487756c924ac22f16938999
Hammad <hammad.rizvi@mmcgbl.com>
2023-01-04 03:05:57 +0500

    push

--- App.js
+++ App.js
@@ -1,34 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import MapView, { enableLatestRenderer , PROVIDER_GOOGLE } from 'react-native-maps';
-
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    // height: 400,
-    width: 400,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-});
+import { View } from 'react-native';
+import Report from './src/screens/Reports.Screen';
 
 function App() {
   return (
-    <View style={styles.container}>
-      <MapView
-        // provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-        style={styles.map}
-        region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}
-      >
-      </MapView>
+    <View style={{flex:1}}>
+      <Report />
     </View>
   )
 }
