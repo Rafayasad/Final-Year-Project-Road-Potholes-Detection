aee4c905d852486aa25fb17a644c3dc4240dc893
Hammad <hammad.rizvi@mmcgbl.com>
2023-01-08 19:54:41 +0500

    hammad

--- src/screens/reports.screen.js
+++ src/screens/reports.screen.js
@@ -15,6 +15,10 @@ import {
   ContributionGraph,
   StackedBarChart,
 } from 'react-native-chart-kit';
+import IconHome from 'react-native-vector-icons/MaterialCommunityIcons';
+import FlatButton from '../components/elements/FlatButton';
+import {Avatar, Button, Card} from 'react-native-paper';
+import {Color} from '../components/theme/colors';
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
@@ -37,7 +41,7 @@ function Report() {
   const chartConfig = {
     backgroundGradientFrom: '#1E2923',
     backgroundGradientFromOpacity: 0,
-    backgroundGradientTo: '#EAF9E7',
+    backgroundGradientTo: 'white',
     backgroundGradientToOpacity: 0.5,
     color: (opacity = 1) => `rgba(1, 50, 55, ${opacity})`,
     strokeWidth: 2, // optional, default 3
@@ -49,11 +53,22 @@ function Report() {
       <ScrollView>
         <View style={styles.container}>
           <View style={styles.road_name_view}>
-            <Text size="xl" weight="heavy" title="Road Name" />
+            <IconHome
+              style={{marginRight: 5}}
+              name="road-variant"
+              color={'#013237'}
+              size={26}
+            />
+            <Text
+              size="xl"
+              weight="heavy"
+              title="Road Name"
+              color={'#013237'}
+            />
           </View>
           <View style={styles.report_view}>
             <View style={styles.graphs_view}>
-              <View style={styles.graph_view}>
+              <Card style={styles.graph_view}>
                 <ProgressChart
                   data={data}
                   width={windowWidth * 0.45}
@@ -63,9 +78,14 @@ function Report() {
                   chartConfig={chartConfig}
                   hideLegend={true}
                 />
-                <Text size="sm" weight="lite" title="80% Poth holes" />
-              </View>
-              <View style={styles.graph_view}>
+                <Text
+                  size="sm"
+                  weight="lite"
+                  title="80% Poth holes"
+                  color={'#013237'}
+                />
+              </Card>
+              <Card style={styles.graph_view}>
                 <LineChart
                   data={data2}
                   width={windowWidth * 0.45}
@@ -74,11 +94,21 @@ function Report() {
                   chartConfig={chartConfig}
                   bezier
                 />
-                <Text size="sm" weight="lite" title="80% Life Span" />
-              </View>
+                <Text
+                  size="sm"
+                  weight="lite"
+                  title="80% Life Span"
+                  color={'#013237'}
+                />
+              </Card>
             </View>
             <View style={styles.map_view}>
-              <Text size="lg" weight="heavy" title="Map View" />
+              <Text
+                size="lg"
+                weight="heavy"
+                title="Map View"
+                color={'#013237'}
+              />
               <MapView
                 style={styles.map}
                 region={{
@@ -96,16 +126,32 @@ function Report() {
               </MapView>
             </View>
             <View style={styles.date_view}>
-              <Text size="sm" weight="heavy" title="Dated :" />
-              <Text size="sm" weight="lite" title="10-20-2018" />
+              <Text
+                size="sm"
+                weight="heavy"
+                title="Dated :"
+                color={'#013237'}
+              />
+              <Text
+                size="sm"
+                weight="lite"
+                title="10-20-2018"
+                color={'#013237'}
+              />
             </View>
-            <View style={styles.download_view}>
-              <TouchableOpacity>
-                <Text size="lg" weight="lite" title="Download Button" />
-              </TouchableOpacity>
-            </View>
+            {/* <View style={styles.download_view}> */}
+
+            {/* </View> */}
           </View>
           {/* <View style={styles.footer}></View> */}
+        </View>
+        <View style={styles.download_view}>
+          <FlatButton
+            text={'Download PDF'}
+            bgColor={Color.dangerColor}
+            textColor={'#EAF9E7'}
+            margintop={5}
+          />
         </View>
       </ScrollView>
     </View>
@@ -129,21 +175,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   road_name_view: {
-    justifyContent: 'center',
+    flexDirection: 'row',
     alignItems: 'center',
-    marginVertical: 10,
+    justifyContent: 'center',
+    marginVertical: 15,
     height: windowHeight * 0.05,
+    // backgroundColor:'red'
   },
   report_view: {
     width: windowWidth,
-    height: windowHeight * 0.9,
+    height: windowHeight * 0.65,
     alignItems: 'center',
+    // backgroundColor:'red'
   },
   graphs_view: {
     width: '100%',
     height: windowHeight * 0.3,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    justifyContent: 'space-around',
   },
   graph_view: {
     width: '48%',
@@ -151,27 +201,26 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     justifyContent: 'space-around',
     alignItems: 'center',
-    backgroundColor: '#EAF9E7',
+    backgroundColor: 'white',
     borderRadius: 10,
   },
   map_view: {
-    width: '100%',
-    height: '30%',
+    width: '95%',
+    height: '40%',
     alignItems: 'center',
     justifyContent: 'space-between',
+    // justifyContent: 'space-around',
   },
   date_view: {
-    width: '90%',
-    height: '3%',
+    width: '95%',
+    height: '20%',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
   },
   download_view: {
-    width: '90%',
-    height: '7%',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: windowWidth,
+    paddingHorizontal:10
   },
   footer: {
     width: windowWidth,
@@ -179,7 +228,8 @@ const styles = StyleSheet.create({
   },
   map: {
     width: '100%',
-    height: '80%',
+    height: '100%',
     borderRadius: 10,
+    marginTop: 10,
   },
 });
