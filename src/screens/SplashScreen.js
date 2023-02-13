ca09498d35b80d7b551bb3df3c0d4e127ac55014
Hammad <hammad.rizvi@mmcgbl.com>
2023-02-13 07:20:09 +0500

    last

---
+++ src/screens/SplashScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, Image} from 'react-native';
+import Text from '../components/elements/Text';
+import Logo from '../../assets/images/Logo.png';
+import {Color} from '../components/theme/colors';
+// import FlatButton from '../components/elements/FlatButton';
+
+const SplashScreen = () => {
+  return (
+    <View
+      flex={1}
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: Color.secondaryColor,
+      }}>
+      <Image source={Logo} />
+      <View style={{marginTop: 20}}>
+        <Text weight="heavy" title="Poth Holes Detection" size="xl" />
+      </View>
+    </View>
+  );
+};
+export default SplashScreen;
